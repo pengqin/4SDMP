ff6abf9297740e41f6651927312e9d0ac2990104
hopesfish <hopesfish@163.com>
2013-12-14 15:26:48 +0800

    add dialog

--- web/routes/wap/profile.js
+++ web/routes/wap/profile.js
@@ -1,12 +1,13 @@
 /*
  * GET register page.
  */
+var user = {mobile: '13811749917'};
 exports.index = function(req, res){
- 	res.render('wap/profile/index', {title: "个人资料"});
+ 	res.render('wap/profile/index', {title: "个人资料", user: user});
 };
 exports.password = function(req, res){
- 	res.render('wap/profile/password', {title: "修改密码"});
+ 	res.render('wap/profile/password', {title: "修改密码", user: user});
 };
 exports.reminder = function(req, res){
- 	res.render('wap/profile/reminder', {title: "提醒服务"});
+ 	res.render('wap/profile/reminder', {title: "提醒服务", user: user});
 };
