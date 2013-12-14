435a448c311ad46772b3b32ffe52c452804d6ad6
hopesfish <hopesfish@163.com>
2013-12-14 14:27:59 +0800

    fix domain issue

--- web/wxbot/gateway/index.js
+++ web/wxbot/gateway/index.js
@@ -10,5 +10,5 @@ module.exports = function(webot) {
 	webot.loads("profile");
 
     // 定义gateway/member/coacher域, 将判断该用户是否可以进行激活操作
-    //webot.domain("gateway", utils.ensure_user_is_register);
+    webot.domain("gateway");
 }
