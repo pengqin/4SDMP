53400a3505b86af6222dc65de39453d03550edee
hopesfish <hopesfish@163.com>
2013-12-14 21:12:12 +0800

    update link

--- web/wxbot/gateway/profile.js
+++ web/wxbot/gateway/profile.js
@@ -31,7 +31,7 @@ module.exports = function(webot) {
 	// 修改个人资料提示语
 	webot.set('user profile links start by text', {
 		domain: "gateway",
-		pattern: /^(1|个人设置)/i,
+		pattern: /^(5|个人设置)/i,
 		handler: profile_features
 	});
 	webot.set('user profile links start by event', {
