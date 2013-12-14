5b334947584468c81f435dddea2acfc83bfdf104
hopesfish <hopesfish@163.com>
2013-12-14 23:54:55 +0800

    add info

--- web/wxbot/4sdmp/index.js
+++ web/wxbot/4sdmp/index.js
@@ -10,4 +10,11 @@ module.exports = function(webot) {
 			next("4S数字营销平台!");
 		}
 	});
+	// 介绍weexiao
+	webot.set('4sdmp brithday', {
+		pattern: /^软件生日$/i,
+		handler: function(info, next) {
+			next("和玉兔登录月球同一天哦!19.51°W 44.12°N");
+		}
+	});
 }
